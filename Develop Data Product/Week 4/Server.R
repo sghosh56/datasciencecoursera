3a11cbf2b507edbbedaad004043f079e62883aba
sghosh56 <30870595+sghosh56@users.noreply.github.com>
2018-03-26 15:34:56 -0400

    Update Server.R

--- Develop Data Product/Week 4/Server.R
+++ Develop Data Product/Week 4/Server.R
@@ -1,1 +1,33 @@
+---
+title: "Server.R"
+author: "Sayak"
+date: "March 25, 2018"
+output: html_document
+runtime: shiny
+---
+# This is the server logic for a Shiny web application.
+# You can find out more about building applications with Shiny here:
+#
+# http://shiny.rstudio.com
+#
+```{R} 
+
+library(shiny)
+
+   shinyServer(function(input, output) {
+   output$distPlot <- renderPlot({
+    dist <- switch(input$dist,
+                   Normal = rnorm,
+                   Uniform = runif,
+                   Lognormal = rlnorm,
+                   Exponential = rexp,
+                   rnorm)
+    
+    hist(dist(input$obs), col = 'lightblue',
+         main = paste("Histogram of a" , input$dist, "Distribution with ", input$obs, " Observations"))  ##dist size, parameter from slider
+  })
+}
+) 
+    
+``` 
 
